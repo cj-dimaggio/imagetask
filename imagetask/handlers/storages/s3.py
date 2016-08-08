c6e953676b94ab652b97392465bd7d267e1dcf7e
CJ DiMaggio <charlesdimaggio@localprojects.net>
2016-08-08 02:44:02 -0400

    Fix S3 Storage

--- imagetask/handlers/storages/s3.py
+++ imagetask/handlers/storages/s3.py
@@ -9,9 +9,10 @@ class S3Storage(S3IO, BaseStorage):
     def save(self, path, img, save_options):
         name = self.get_name(path)
         key = self.bucket.new_key(name)
-        key.set_acl(self.config.ACL)
         f = StringIO()
         img.save(f, format=img.format, **save_options)
+        f.seek(0)
         key.set_contents_from_file(f)
+        key.set_acl(self.config.ACL)
         f.seek(0)
         return f
