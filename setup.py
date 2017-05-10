4faf168fb10f5c2b82184c547636b74b87dae528
CJ DiMaggio <charlesdimaggio@localprojects.net>
2017-05-10 12:59:45 -0400

    Increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ packages.remove('tests')
 
 setup(
     name='ImageTask',
-    version='0.0.2',
+    version='0.0.3',
     author='Local Projects',
     description="An image processing service.",
     packages=packages,
